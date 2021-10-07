af4fe85bdc5dae350ec1272ba442488701ad3ca8
Matt <mattdev001@gmail.com>
2021-10-07 18:08:36 +1100

    tidy

--- scripts/src/setup.ts
+++ scripts/src/setup.ts
@@ -77,6 +77,8 @@ const setup = async () => {
   console.log("-------------");
   const testkey0 = fs.readFileSync(`./keys/alice_pub.json`) as unknown as string;
   console.log(testkey0);
+  console.log(testkey0.constructor.name);
+  console.log(testkey0 instanceof ArrayBuffer);
   const testkey05 = fs.readFileSync(`./keys/alice_pub.json`);
   console.log(testkey05);
   const testkey = JSON.parse(fs.readFileSync(`./keys/alice_pub.json`) as unknown as string);
