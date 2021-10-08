c0a87427d258b929a3e73ace17f4750c6c8690bc
Matt <mattdev001@gmail.com>
2021-10-08 22:06:31 +1100

    tidy

--- scripts/src/setup.ts
+++ scripts/src/setup.ts
@@ -123,9 +123,8 @@ const setup = async () => {
 			console.log('Connection to cluster established:', rpcUrl, version);
 }
 
-const newConnection = await establishConnection() => (
-      return connection;
-);
+const newConnection = await establishConnection() => {};
+      //return connection;
 
     //const connection = new Connection("https://api.testnet.solana.com", "confirmed");
  const connection = new Connection("http://localhost:8899", "confirmed");
