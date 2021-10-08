44cbcb78ca0764af9ca3a7c283e650c2960ec6f9
Matt <mattdev001@gmail.com>
2021-10-09 02:14:13 +1100

    tidy

--- scripts/src/setup.ts
+++ scripts/src/setup.ts
@@ -49,7 +49,7 @@ const setupMint = async (
   console.log(clientKeypair.publicKey);
   console.log("mint public key ...");
   console.log(mint.publicKey);
-  const mint_pub = JSON.stringify(mint.publicKey);
+  const mint_pub = JSON.stringify(mint.publicKey.tobase58());
   console.log(mint_pub);
   console.log("mint object");
   console.log(mint);
